58313ac2cbbdd9f83c29e75da14fc9f697a286cf
baby7305 <songhaibei7305@163.com>
2021-08-26 15:16:12 +0800

    写入csv文件

--- quickStart.py
+++ quickStart.py
@@ -18,6 +18,25 @@ df = pd.read_csv('nba.csv')
 print(df)
 
 
+# In[7]:
+
+
+import pandas as pd
+   
+# 三个字段 name, site, age
+nme = ["Google", "Runoob", "Taobao", "Wiki"]
+st = ["www.google.com", "www.runoob.com", "www.taobao.com", "www.wikipedia.org"]
+ag = [90, 40, 80, 98]
+   
+# 字典
+dict = {'name': nme, 'site': st, 'age': ag}
+     
+df = pd.DataFrame(dict)
+ 
+# 保存 dataframe
+df.to_csv('site.csv')
+
+
 # In[ ]:
 
 
