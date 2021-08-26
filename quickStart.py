0e15fb71091fbd9a133e45afbc418a1b9060dac5
baby7305 <songhaibei7305@163.com>
2021-08-26 16:09:12 +0800

    自定义索引

--- quickStart.py
+++ quickStart.py
@@ -61,6 +61,21 @@ df = pd.DataFrame(data)
 print(df.loc[[0, 1]])
 
 
+# In[22]:
+
+
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df)
+
+
 # In[ ]:
 
 
