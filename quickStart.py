c440469c950e2306efc3578fe230ff1716d79c80
baby7305 <songhaibei7305@163.com>
2021-08-26 14:48:05 +0800

    简单的pandas实例

--- quickStart.py
+++ quickStart.py
@@ -8,6 +8,21 @@ import pandas
 pandas.__version__
 
 
+# In[5]:
+
+
+import pandas as pd
+
+mydataset = {
+  'sites': ["Google", "Runoob", "Wiki"],
+  'number': [1, 2, 3]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+
 # In[ ]:
 
 
