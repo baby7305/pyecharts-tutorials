0f5bf23615f45e10fcf977f1645f325dd1f24ca3
baby7305 <songhaibei7305@163.com>
2021-08-26 16:00:58 +0800

    单行索引

--- quickStart.py
+++ quickStart.py
@@ -44,6 +44,25 @@ df = pd.DataFrame(data)
 print (df)
 
 
+# In[20]:
+
+
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+# 数据载入到 DataFrame 对象
+df = pd.DataFrame(data)
+
+# 返回第一行
+print(df.loc[0])
+# 返回第二行
+print(df.loc[1])
+
+
 # In[ ]:
 
 
