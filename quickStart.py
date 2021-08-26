48c23837a394b5cad4891d9b95074e34e8b82912
baby7305 <songhaibei7305@163.com>
2021-08-26 15:50:56 +0800

    使用列表创建二维数组

--- quickStart.py
+++ quickStart.py
@@ -8,6 +8,18 @@ import pandas
 pandas.__version__
 
 
+# In[17]:
+
+
+import pandas as pd
+
+data = [['Google',10],['Runoob',12],['Wiki',13]]
+
+df = pd.DataFrame(data,columns=['Site','Age'])
+
+print(df)
+
+
 # In[ ]:
 
 
