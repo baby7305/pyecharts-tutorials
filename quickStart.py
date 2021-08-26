2b9475b70977ef813761a54316c074eeaf68ec63
baby7305 <songhaibei7305@163.com>
2021-08-26 15:56:45 +0800

    使用字典创建二维数组

--- quickStart.py
+++ quickStart.py
@@ -32,6 +32,18 @@ df = pd.DataFrame(data)
 print (df)
 
 
+# In[19]:
+
+
+import pandas as pd
+
+data = [{'a': 1, 'b': 2},{'a': 5, 'b': 10, 'c': 20}]
+
+df = pd.DataFrame(data)
+
+print (df)
+
+
 # In[ ]:
 
 
